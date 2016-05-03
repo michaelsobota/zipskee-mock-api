c7cd51afb51e41cec841122828178c819b1df6ee
Michael Sobota <michael.sobota@charter.com>
2016-05-02 21:57:11 -0600

    reinit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var middleware = require('swagger-express-middleware');
+ 
+var app = express();
+ 
+middleware('zipskee-v1.swagger.yaml', app, function(err, middleware) {
+    // Add all the Swagger Express Middleware, or just the ones you need. 
+    // NOTE: Some of these accept optional options (omitted here for brevity) 
+    app.use(
+        middleware.metadata(),
+        middleware.CORS(),
+        middleware.files(),
+        middleware.parseRequest(),
+        middleware.validateRequest(),
+        middleware.mock()
+    );
+    app.use('/swagger', express.static('./swagger-ui/dist'));
+    app.listen(8000, function() {
+        console.log('The zipskee sample is now running at http://localhost:8000');
+    });
+});
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Zipskee Mock API
+## Getting Started
+First, clone this repo
+
+Then...
+`npm install`
+
+To run:
+`node index.js`
+
+To access the pretty spec docs:
+`http://localhost:8000/swagger`
+
+This is a (nearly)fully-functional mock, including data persistence. So any POST you make,  you'll be able to GET that data back. Data is NOT kept once the server is killed.
